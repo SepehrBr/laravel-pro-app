518fe80b03d2244df88abfb6c187f235dd07ea37
unknown <sepehrborna1999@gmail.com>
2024-12-01 20:00:19 +0330

    email verification

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->middleware(['auth', 'verified']);
 
-Auth::routes();
+Auth::routes([
+    'verify'=> true,
+]);
 
 Route::get('/auth/google', [GoogleAuthController::class, 'redirect'])->name('auth.google');
 Route::get('/auth/google/callback', [GoogleAuthController::class, 'callback']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// password confirm
+// password confirm and email confirm
 Route::get('secret', function () {
     return 'secret';
-})->middleware(['auth', 'password.confirm']);
+})->middleware(['auth', 'password.confirm', 'verified']);
