d668cd387c1ef734a11d95911ae02c69e6a3e051
unknown <sepehrborna1999@gmail.com>
2024-12-09 19:12:13 +0330

    add queue to logintowebsite notification

--- app/Notifications/LoginToWebNotification.php
+++ app/Notifications/LoginToWebNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class LoginToWebNotification extends Notification
+class LoginToWebNotification extends Notification implements ShouldQueue 
 {
     use Queueable;
 
